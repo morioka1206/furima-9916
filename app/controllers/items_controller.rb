10a4696328a6e3e420a8b08560e343df4ec35ab7
morioka1206 <r.morioka@icloud.com>
2021-03-05 20:42:16 +0900

    商品削除機能4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index ,:show]
   before_action :item_param, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
     @items = Item.order(id: "DESC")
